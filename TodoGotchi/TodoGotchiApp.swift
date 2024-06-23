cee56c0b2cc42f1de1f089b4435aa1a7e62c614a
Christopher Babayan <babayanchristopher@gmail.com>
2024-06-22 17:30:30 -0700

    Initial Commit

---
+++ TodoGotchi/TodoGotchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoGotchiApp.swift
+//  TodoGotchi
+//
+//  Created by Christopher Babayan on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoGotchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
